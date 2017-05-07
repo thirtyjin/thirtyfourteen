6bac44dc731178d0be6d68e4c217734b60e8ba32
thirty <thirty.jin@gmail.com>
2017-05-07 11:51:19 +0800

    首次提交全部代码

---
+++ content-portfolio.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * The default template for displaying content
+ *
+ * Used for both single and index/archive/search.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Fourteen
+ * @since Twenty Fourteen 1.0
+ */
+?>
+
+<article id="post-<?php the_ID(); ?>" class="project-cover" <?php post_class(); ?>>
+	
+	<a class="post-thumbnail" href="<?php the_permalink(); ?>">
+		<div class="project-cover-thumb" style="background-image: url(<?php the_post_thumbnail_url(); ?>); ">
+			<?php //twentyfourteen_post_thumbnail(); ?>
+		</div>
+	</a>
+
+
+	<header class="project-cover-title"> 
+
+		<?php
+			if ( is_single() ) :
+				the_title( '<h1 class="entry-title">', '</h1>' );
+			else :
+				the_title( '<h1 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h1>' );
+			endif;
+		?>
+
+	</header><!-- .entry-header -->
+
+	<div class="project-cover-meta entry-meta ">
+		<?php  echo get_the_term_list($post->ID, 'project-type', '<span class="cat-links">', ', ','</span>'); ?>
+
+		<?php the_date( 'Y', '<span>', '</span>' ); ?>
+
+		<?php edit_post_link( __( 'Edit', 'twentyfourteen' ), '<span class="edit-link">', '</span>' ); ?>
+	</div>
+
+
+</article><!-- #post-## -->
---
+++ content-single-portfolio.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * The default template for displaying content
+ *
+ * Used for both single and index/archive/search.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Fourteen
+ * @since Twenty Fourteen 1.0
+ */
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	
+	<?php //twentyfourteen_post_thumbnail(); ?>
+
+	<header class="responsive1 entry-header">
+	
+		<?php
+			if ( is_single() ) :
+				the_title( '<h1 class="entry-title">', '</h1>' );
+			else :
+				the_title( '<h1 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h1>' );
+			endif;
+		?>
+
+	<div class="project-cover-meta entry-meta ">
+		<?php  echo get_the_term_list($post->ID, 'project-type', '<span class="cat-links">', ', ','</span>'); ?>
+
+		<?php the_date( 'Y', '<span>', '</span>' ); ?>
+		
+		<?php edit_post_link( __( 'Edit', 'twentyfourteen' ), '<span class="edit-link">', '</span>' ); ?>
+	</div>
+	
+	</header><!-- .entry-header -->
+
+
+
+	<?php if ( is_search() ) : ?>
+	<div class="responsive1 entry-summary">
+		<?php the_excerpt(); ?>
+	</div><!-- .entry-summary -->
+	<?php else : ?>
+	<div class="responsive1 entry-content">
+		<?php
+			/* translators: %s: Name of current post */
+			the_content( sprintf(
+				__( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'twentyfourteen' ),
+				the_title( '<span class="screen-reader-text">', '</span>', false )
+			) );
+
+			wp_link_pages( array(
+				'before'      => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'twentyfourteen' ) . '</span>',
+				'after'       => '</div>',
+				'link_before' => '<span>',
+				'link_after'  => '</span>',
+			) );
+		?>
+	</div><!-- .entry-content -->
+	<?php endif; ?>
+	
+	<footer class="entry-footer">
+		<div class="entry-meta">
+			<span class="tag-links">
+				<?php the_tags( '', '+', '' ); ?>
+			</span>
+		</div>
+	</footer><!-- .entry-footer -->
+
+</article><!-- #post-## -->
